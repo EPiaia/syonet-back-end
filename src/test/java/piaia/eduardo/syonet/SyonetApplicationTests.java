7667f72a97a234b4db64fc80b8e10a01ea457fdd
Eduardo Piaia <eduardopiaia51@gmail.com>
2024-03-20 12:32:00 -0300

    Estruturação inicial do projeto

---
+++ src/test/java/piaia/eduardo/syonet/SyonetApplicationTests.java
@@ -0,0 +1,13 @@
+package piaia.eduardo.syonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
