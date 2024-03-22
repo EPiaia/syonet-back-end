1e1655f79ef2384c104f65b6c02658e4714fedae
Eduardo Piaia <eduardopiaia51@gmail.com>
2024-03-22 12:57:36 -0300

    Ajuste nas configurações: - Implementado para considerar localhost:3000 nas configurações de CORS do Spring.

--- src/main/java/piaia/eduardo/syonet/controller/AuthController.java
+++ src/main/java/piaia/eduardo/syonet/controller/AuthController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @CrossOrigin
     @PostMapping
     public ResponseEntity<User> login(@RequestBody @Valid LoginRequest loginRequest) {
         User user = userRepository.findUserByUsername(loginRequest.username());
