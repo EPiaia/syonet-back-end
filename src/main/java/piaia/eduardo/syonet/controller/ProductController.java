6a20af01578c17b11a69806c8bbecb7b74657707
Eduardo Piaia <eduardopiaia51@gmail.com>
2024-03-24 20:56:08 -0300

    Implementações: - Implementado endpoint de put nos produtos; - Implementado endpoint de delete nos produtos; - Implementada pesquisa de todos produtos ordenado pelo ID asc.

--- src/main/java/piaia/eduardo/syonet/controller/ProductController.java
+++ src/main/java/piaia/eduardo/syonet/controller/ProductController.java
@@ -6,9 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +27,7 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productRepository.findAll();
+        List<Product> products = productRepository.findAllByOrderByIdAsc();
         if (products.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
@@ -42,6 +44,30 @@ public class ProductController {
     public ResponseEntity<Product> save(@RequestBody @Valid Product product) {
         Product persistedProduct = productRepository.save(product);
         return ResponseEntity.ok(persistedProduct);
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> update(@RequestBody @Valid Product product) {
+        Product existentProduct = productRepository.findById(product.getId()).orElse(null);
+        if (existentProduct == null) {
+            return ResponseEntity.notFound().build();
+        }
+        existentProduct.setName(product.getName());
+        Product persistedProduct = productRepository.save(existentProduct);
+        
+        return ResponseEntity.ok(persistedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> delete(@PathVariable Integer id) {
+        Product existentProduct = productRepository.findById(id).orElse(null);
+        if (existentProduct == null) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        productRepository.delete(existentProduct);
+
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/piaia/eduardo/syonet/repository/ProductRepository.java
+++ src/main/java/piaia/eduardo/syonet/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package piaia.eduardo.syonet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import piaia.eduardo.syonet.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    public List<Product> findAllByOrderByIdAsc();
+
 }
